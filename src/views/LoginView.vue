6bca4ae6539f0edc3bd7196dcda2c4d8434269e3
work <temogogitidze@redberry.ge>
2023-06-08 00:09:08 +0400

    feat: add axios plugins instance, import in component

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pt-16">
     <h1 class="text-3xl font-semibold mb-4">Enter your phone number</h1>
-    <form action="#">
+    <form action="#" @submit.prevent="handleLogin()">
       <div class="overflow-hidden shadow sm:rounded-md max-w-sm mx-auto text-left">
         <div class="bg-white px-4 py-5 sm:p-6">
           <div>
@@ -11,6 +11,7 @@
               data-maska="+ (###) ###-##-##-##"
               name="phone"
               id="phone"
+              v-model="credentials.phone"
               placeholder="+ (995) 94-32-99"
               class="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm focus:border-black focus:outline-none"
             />
@@ -19,6 +20,7 @@
         <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
           <button
             type="submit"
+            @submit.prevent="handleLogin()"
             class="inline-flex justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-gray-600 focus:outline-none"
           >
             Continue
@@ -30,6 +32,24 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { reactive } from 'vue'
 import { vMaska } from 'maska'
+import defaultInstance from '@/plugins/axios/axios'
+
+const credentials = reactive({
+  phone: null
+})
+
+const handleLogin = () => {
+  defaultInstance
+    .post('/login', credentials)
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+      alert(err.response.data.message)
+    })
+  console.log(credentials.phone)
+}
 </script>
