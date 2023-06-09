24ab5c96bc1daefef4368c7b44ff9f67882733f9
work <temogogitidze@redberry.ge>
2023-06-09 16:55:59 +0400

    refactor: export reusable functions in composable

--- src/composables/location.js
+++ src/composables/location.js
@@ -1,12 +1,20 @@
 import { httpPostRequest } from '@/helpers/http/httpRequest'
+import { useRouter } from 'vue-router'
 
 export const useLocation = () => {
-  const handleConfirmTrip = (location) => {
-    httpPostRequest('/trip', {
-      origin: location.current.geometry,
-      destination: location.destination.geometry,
-      destination_name: location.destination.name
-    })
+  const router = useRouter()
+
+  const handleConfirmTrip = async (location) => {
+    try {
+      await httpPostRequest('/trip', {
+        origin: location.current.geometry,
+        destination: location.destination.geometry,
+        destination_name: location.destination.name
+      })
+      router.push({ name: 'trip' })
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return { handleConfirmTrip }
